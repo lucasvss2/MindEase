3f417e10e5cf287ecbf23bb6e263a9cd17f28579
Thalya Steffany <contato.thalyasteffany@gmail.com>
2026-01-20 16:41:35 -0300

    chore: initialize main folders(clean architecture)

---
+++ mind-ease-mobile/src/presentation/pages/TaskPage/index.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export function TaskPage() {
+    return <View><Text>Task Page</Text></View>
+}
---
+++ mind-ease-mobile/src/presentation/providers/MainProvider.tsx
@@ -0,0 +1,16 @@
+ 
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+
+export const MainProvider = ({ children }: { children: ReactNode }) => {
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+    </QueryClientProvider>
+  );
+};
