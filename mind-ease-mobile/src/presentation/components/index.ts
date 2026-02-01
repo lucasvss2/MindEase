c2cf4f748f53018a932c58e813c49689dbe8f948
davimattos <davi.mattos@thoughtworks.com>
2026-02-01 00:27:53 -0300

    feat(ui): add ColorPicker component

--- mind-ease-mobile/src/presentation/components/index.ts
+++ mind-ease-mobile/src/presentation/components/index.ts
@@ -2,6 +2,7 @@ export * from './Avatar';
 export * from './BoardCard';
 export * from './Button';
 export * from './Card';
+export * from './ColorPicker';
 export * from './Dropdown';
 export * from './Dropdown/components';
 export * from './ScreenHeader';
