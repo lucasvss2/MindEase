d4fa067af40735818e5dd0fddc91eade9119c9b6
Lucas Siqueira <54068176+lucasvss2@users.noreply.github.com>
2026-02-07 11:37:19 -0300

    feat/update styling library and changing to clean architecture

--- mind-ease-web/src/presentation/components/shared/Layout/PageLayout/Header/index.tsx
+++ mind-ease-web/src/presentation/components/shared/Layout/PageLayout/Header/index.tsx
@@ -10,6 +10,7 @@ import {
 import {
   useToggle
 } from '@/presentation'
+import logo from '@/assets/images/mind-ease-icon.png'
 
 import * as S from './styles'
 
@@ -23,6 +24,7 @@ export const Header = () => {
     <S.Container>
       <S.LogoContainer onClick={() => { navigate('/') }}>
         <S.LogoTextContainer>
+          <img src={logo} alt="MindEase-logo" width={30} height={30} />
           <S.LogoText>MindEase</S.LogoText>
         </S.LogoTextContainer>
       </S.LogoContainer>
--- mind-ease-web/src/presentation/pages/app/LandingPage/index.tsx
+++ mind-ease-web/src/presentation/pages/app/LandingPage/index.tsx
@@ -2,6 +2,11 @@ import { PageLayout } from '@/layouts'
 
 export function LandingPage() {
   return (
-    <PageLayout>LandingPage</PageLayout>
+    <PageLayout>
+      <section>
+        <h1>MindEase</h1>
+        <p>MindEase is a mental health platform that provides resources and support for people who are struggling with mental health issues.</p>
+      </section>
+    </PageLayout>
   )
 }
