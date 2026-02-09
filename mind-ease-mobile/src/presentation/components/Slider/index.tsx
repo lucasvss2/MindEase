b47440e6203246b606e5b0ac02b214df3c7b7981
Thalya Steffany <studies.thalya@email.com>
2026-02-09 12:36:07 -0300

    feat: implement contrast variants for Button, Slider and Switch components

--- mind-ease-mobile/src/presentation/components/Slider/index.tsx
+++ mind-ease-mobile/src/presentation/components/Slider/index.tsx
@@ -1,11 +1,30 @@
+import { TOKENS } from "@/presentation/constants";
+import useUserPreferencesStore from "@/presentation/store/useUserPreferencesStore";
 import SliderCommunity, { SliderProps } from "@react-native-community/slider";
 
 export const Slider: React.FC<SliderProps> = (props) => {
+  const { contrast } = useUserPreferencesStore();
+
+  const styleByContrast = {
+    low: {
+      minimumTrackTintColor: TOKENS.COLORS.blue[750],
+      thumbTintColor: TOKENS.COLORS.blue[750],
+    },
+    moderate: {
+      minimumTrackTintColor: TOKENS.COLORS.blue[725],
+      thumbTintColor: TOKENS.COLORS.blue[725],
+    },
+    high: {
+      minimumTrackTintColor: TOKENS.COLORS.blue[900],
+      thumbTintColor: TOKENS.COLORS.blue[900],
+    },
+  };
+
   return (
     <SliderCommunity
-      minimumTrackTintColor='#0091ffe9'
-      maximumTrackTintColor='#8A8585'
-      thumbTintColor='#008FFF'
+      {...styleByContrast[contrast]}
+      maximumTrackTintColor={TOKENS.COLORS.neutral[350]}
+      className='p-10'
       {...props}
     />
   );
