a68e9b43c1ddf73a48ac7f99306dac340050ec07
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-02-13 20:33:40 -0300

    chore(validations): create schemas for 'Login' and 'Create Account' password

---
+++ mind-ease-mobile/src/utils/validations/authSchemas.ts
@@ -0,0 +1,33 @@
+import * as yup from "yup";
+
+export const passwordSchema = yup.object({
+  password: yup
+    .string()
+    .required("O campo é obrigatório!")
+    .min(6, "A senha deve ter no mínimo 6 caracteres.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).*$/,
+      "Sua senha não atende aos requisitos. Consulte o ícone i ao lado do campo para mais detalhes.",
+    ),
+});
+
+export const emailSchema = yup.object({
+  email: yup
+    .string()
+    .email(
+      "Email inválido. Por favor, insira um email no formato email@email.com",
+    )
+    .required("O Campo é obrigatório!"),
+});
+
+export const loginSchema = passwordSchema
+  .concat(passwordSchema)
+  .concat(emailSchema);
+
+export const createAccountSchema = yup
+  .object({
+    fullname: yup.string().required("O Campo é obrigatório!"),
+  })
+  .concat(passwordSchema)
+  .concat(emailSchema);
+
