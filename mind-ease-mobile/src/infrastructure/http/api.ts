e994c62455dfc9fe07cbb09786798a47fe26ffed
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-02-14 13:24:13 -0300

    fix(http): add accept and user agent headers

--- mind-ease-mobile/src/infrastructure/http/api.ts
+++ mind-ease-mobile/src/infrastructure/http/api.ts
@@ -1,22 +1,21 @@
-import useAuthStore from "@/presentation/store";
+import useAuthStore from "@/presentation/store/useAuthStore";
 import axios from "axios";
+console.log("baseURL: ", process.env.EXPO_PUBLIC_API_URL);
 
 export const api = axios.create({
   baseURL: `${process.env.EXPO_PUBLIC_API_URL}`,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
+    "User-Agent": "RN-App-Client",
   },
 });
 
-api.interceptors.request.use(
-  config => {
-    const { token } = useAuthStore.getState();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
+
