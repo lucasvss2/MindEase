c03f2ecf68ea1946fa1c2c02c2d08ff8a42bfcd5
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-02-21 16:51:28 -0300

    feat(components): createSharedModalBase

---
+++ mind-ease-mobile/src/presentation/components/SharedModalBase/interface.ts
@@ -0,0 +1,13 @@
+export interface ISharedModalBase  {
+  labels: { key: string; label: string }[];
+  children: React.ReactNode;
+  saveText: string;
+  disabled?: boolean;
+  title: string;
+  snapPoints?: [number, number];
+  onCancelAction: () => void;
+  onSubmitChanges: () => void;
+  isLoading?: boolean;
+  visible: boolean;
+}
+
