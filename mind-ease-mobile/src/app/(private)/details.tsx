20e5d6b3b4795aa18edcb45d4538277fc2c1e273
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-02-21 16:57:30 -0300

    fix(app/private): add safearea

--- mind-ease-mobile/src/app/(private)/details.tsx
+++ mind-ease-mobile/src/app/(private)/details.tsx
@@ -1,5 +1,13 @@
 import { Details } from "@/presentation/pages/Details";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function DetailsScreen() {
-  return <Details />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className='flex-1'>
+        <Details />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
+
--- mind-ease-mobile/src/app/(private)/tasks.tsx
+++ mind-ease-mobile/src/app/(private)/tasks.tsx
@@ -1,5 +1,13 @@
 import { Tasks } from "@/presentation/pages/Tasks";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function TasksScreen() {
-  return <Tasks />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1">
+        <Tasks />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
+
