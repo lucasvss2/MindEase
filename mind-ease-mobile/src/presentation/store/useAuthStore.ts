94b1de19fd6126a25b42cc4ebf83708d6c2aaea5
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-03-05 21:24:44 -0300

    refactor(useAuthStore): move secureStorage to helpers folder

--- mind-ease-mobile/src/presentation/store/useAuthStore.ts
+++ mind-ease-mobile/src/presentation/store/useAuthStore.ts
@@ -1,10 +1,5 @@
-import * as SecureStore from "expo-secure-store";
-import {
-  createJSONStorage,
-  devtools,
-  persist,
-  StateStorage,
-} from "zustand/middleware";
+import { secureStorage } from "@/utils/helpers/secureStorage";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import { shallow } from "zustand/shallow";
 import { createWithEqualityFn } from "zustand/traditional";
 
@@ -15,9 +10,9 @@ type States = {
 };
 
 interface Actions {
-  setEmail: (email: string |  undefined | null) => void;
-  setToken: (token: string |  undefined | null) => void;
-  setRefreshToken: (refreshToken: string |  undefined | null) => void;
+  setEmail: (email: string | undefined | null) => void;
+  setToken: (token: string | undefined | null) => void;
+  setRefreshToken: (refreshToken: string | undefined | null) => void;
   reset: () => void;
 }
 
@@ -27,26 +22,15 @@ const INITIAL_STATE: States = {
   refreshToken: undefined,
 };
 
-const secureStorage: StateStorage = {
-  getItem: async (name: string): Promise<string | null> => {
-    return (await SecureStore.getItemAsync(name)) || null;
-  },
-  setItem: async (name: string, value: string): Promise<void> => {
-    await SecureStore.setItemAsync(name, value);
-  },
-  removeItem: async (name: string): Promise<void> => {
-    await SecureStore.deleteItemAsync(name);
-  },
-};
-
 const useAuthStore = createWithEqualityFn<States & Actions>()(
   devtools(
     persist(
       (set) => ({
         ...INITIAL_STATE,
-        setEmail: (email: string |  undefined | null) => set({ email }),
-        setToken: (token: string |  undefined | null) => set({ token }),
-        setRefreshToken: (refreshToken: string |  undefined | null) => set({ refreshToken }),
+        setEmail: (email: string | undefined | null) => set({ email }),
+        setToken: (token: string | undefined | null) => set({ token }),
+        setRefreshToken: (refreshToken: string | undefined | null) =>
+          set({ refreshToken }),
         reset: () => set(INITIAL_STATE),
       }),
 
