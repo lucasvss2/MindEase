d72fcb5f1e4184674839153d5bf11efab653f2fd
Thalya Steffany <thalyacoiimbra@gmail.com>
2026-03-05 21:31:27 -0300

    chore(repositories): add new function to IColumnRepository

--- mind-ease-mobile/src/domain/respositories/IColumnRepository.ts
+++ mind-ease-mobile/src/domain/respositories/IColumnRepository.ts
@@ -5,6 +5,7 @@ export interface IColumnRepository {
   getColumns: () => Promise<ColumnModel[]>;
   getColumnById: (id: string) => Promise<ColumnModel>;
   getColumnBySlug: (slug: string) => Promise<ColumnModel>;
+  getColumnsByBoardId:  (boardId: string) => Promise<ColumnModel[]>;
   createColumn: (data: CreateColumnDTO) => Promise<ColumnModel>;
   updateColumn: (id: string, data: UpdateColumnDTO) => Promise<ColumnModel>;
   deleteColumn: (id: string) => Promise<void>;
